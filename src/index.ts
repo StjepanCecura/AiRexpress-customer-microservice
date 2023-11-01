e0ac6743abe873332d19ce5ffe377769862048ff
mfric20 <100581678+mfric20@users.noreply.github.com>
2023-11-01 19:30:53 +0100

    Added cors

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
+
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
   res.send("User service running!!");
