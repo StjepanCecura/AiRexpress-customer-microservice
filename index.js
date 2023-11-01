e5b9ac1af0b6a02ef15b505548a04cfc43d6110c
mfric20 <100581678+mfric20@users.noreply.github.com>
2023-11-01 23:18:22 +0100

    Created API for registration
    - Added bcrypt for hashing
    - Added createCustomer function in service for creation a new customer
    - Added POST route "/" for registration

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
-const { getAllCustomers } = require("./services/customer_service.js");
+const { createCustomer } = require("./services/customer_service.js");
 
 dotenv.config();
 
@@ -11,8 +11,11 @@ const port = process.env.PORT;
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  getAllCustomers();
+app.post("/", async (req, res) => {
+  const userData = req.body;
+  const response = await createCustomer(userData);
+
+  res.send(response);
 });
 
 app.listen(port, () => {
--- services/customer_service.js
+++ services/customer_service.js
@@ -1,16 +1,38 @@
 const { client } = require("../utils/commercetools.js");
+const bcrypt = require("bcrypt");
 
-const getAllCustomers = async () => {
+const createCustomer = async (userData) => {
   try {
+    const hashedPassword = await bcrypt.hash(userData.password, 10);
+
+    const newCustomer = {
+      firstName: userData.name,
+      lastName: userData.lastName,
+      email: userData.email,
+      password: hashedPassword,
+      custom: {
+        type: {
+          key: "customer-phone",
+          typeId: "type",
+        },
+        fields: {
+          phone: {
+            en: userData.phone,
+          },
+        },
+      },
+    };
+
     const response = await client.execute({
-      method: "GET",
+      method: "POST",
       uri: "/airtim1-webshop-i-cms/customers",
+      body: newCustomer,
     });
 
-    console.log(response.body);
+    return { status: "OK", message: "New customer created" };
   } catch (error) {
-    console.error(error);
+    return { error };
   }
 };
 
-module.exports = { getAllCustomers };
+module.exports = { createCustomer };
