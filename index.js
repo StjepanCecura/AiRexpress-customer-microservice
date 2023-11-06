678387eb11b70d67b1b1772e0eda5728c2475644
mfric20 <100581678+mfric20@users.noreply.github.com>
2023-11-06 15:51:45 +0100

    Created login route
    - Create login route
    - Created API for login
    - Created login for login and error handeling

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const dotenv = require("dotenv");
 const cors = require("cors");
 const emailVerificationRoute = require("./routes/emailVerificationRoute.js");
 const createCustomerRoute = require("./routes/createCustomerRoute.js");
+const loginCustomerRoute = require("./routes/loginCustomerRoute.js");
 
 dotenv.config();
 
@@ -12,9 +13,10 @@ const port = process.env.PORT;
 app.use(cors());
 app.use(express.json());
 
-app.post("/", createCustomerRoute);
+app.post("/register", createCustomerRoute);
+app.post("/login", loginCustomerRoute);
 app.put(`/email-verification`, emailVerificationRoute);
 
 app.listen(port, () => {
-  console.log(`User service on port: ${port}`);
+  console.log(`Customer service on port: ${port}`);
 });
---
+++ routes/loginCustomerRoute.js
@@ -0,0 +1,33 @@
+const { client } = require("../utils/commercetools.js");
+const dotenv = require("dotenv");
+const jwt = require("jsonwebtoken");
+
+dotenv.config();
+
+const jwtSecret = process.env.JWT_SECRET;
+
+module.exports = async (req, res) => {
+  const userData = req.body;
+
+  try {
+    const response = await client.execute({
+      method: "POST",
+      uri: "/airtim1-webshop-i-cms/me/login",
+      body: userData,
+    });
+
+    const token = jwt.sign(response, jwtSecret, { expiresIn: "2h" });
+
+    res.cookie("token", token, {
+      httpOnly: true,
+    });
+
+    res.redirect("/");
+  } catch (error) {
+    res.send({
+      status: 400,
+      message: "Account with the given credentials not found!",
+    });
+    return;
+  }
+};
