33a093bb46b980f6cd954f4fc287bb929f2c1746
mfric20 <100581678+mfric20@users.noreply.github.com>
2023-12-16 18:11:15 +0100

    Added API routes for customer addresses

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ const JWTMiddleware = require("./middlewares/JWTMiddleware.js");
 const profileRoute = require("./routes/profileRoute.js");
 const signOutRoute = require("./routes/signOutRoute.js");
 const verifyJWTRoute = require("./routes/verifyJWTRoute.js");
+const addCustomerAddressRoute = require("./routes/addCustomerAddressRoute.js");
+const getCustomerAddressRoute = require("./routes/getCustomerAddressRoute.js");
+const changeCustomerAddressRoute = require("./routes/changeCustomerAddressRoute.js");
 
 dotenv.config();
 
@@ -27,12 +30,15 @@ app.use(express.json());
 
 app.get("/", JWTMiddleware, profileRoute);
 app.get("/sign-out", JWTMiddleware, signOutRoute);
+app.get("/getCustomerAddress", getCustomerAddressRoute);
 
 app.post("/register", createCustomerRoute);
 app.post("/login", loginCustomerRoute);
 app.post("/verifyJWT", verifyJWTRoute);
+app.post("/addCustomerAddress", addCustomerAddressRoute);
 
 app.put(`/email-verification`, emailVerificationRoute);
+app.put("/changeCustomerAddress", changeCustomerAddressRoute);
 
 app.listen(port, () => {
   console.log(`Customer service on port: ${port}`);
---
+++ routes/addCustomerAddressRoute.js
@@ -0,0 +1,63 @@
+const dotenv = require("dotenv");
+const jwt = require("jsonwebtoken");
+const { client } = require("../utils/commercetools.js");
+
+dotenv.config();
+
+const jwtSecret = process.env.JWT_SECRET;
+
+module.exports = async (req, res) => {
+  try {
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+    const email = req.body.email;
+    const phoneNumber = req.body.phoneNumber;
+    const country = req.body.country;
+    const city = req.body.city;
+    const postalCode = req.body.postalCode;
+    const streetName = req.body.streetName;
+    const streetNumber = req.body.streetNumber;
+
+    const token = req.cookie.token;
+    const customerData = jwt.verify(token, jwtSecret);
+    const customerId = customerData?.id;
+    const version = customerData?.version;
+
+    const response = await client.execute({
+      method: "POST",
+      uri: `/airexpress/customers/${customerId}`,
+      body: {
+        version,
+        actions: [
+          {
+            action: "addAddress",
+            address: {
+              streetName,
+              streetNumber,
+              postalCode,
+              city,
+              country,
+              phone: phoneNumber,
+              email,
+              firstName,
+              lastName,
+            },
+          },
+        ],
+      },
+    });
+
+    const newToken = jwt.sign(response.body, jwtSecret, {
+      expiresIn: "2h",
+    });
+
+    res.cookie("token", newToken, {
+      httpOnly: true,
+    });
+
+    res.status(200).send({ success: true });
+  } catch (error) {
+    console.log(`Error while updating profile info! ${error}`);
+    res.status(503).send({ error });
+  }
+};
